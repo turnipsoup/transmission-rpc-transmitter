442dbf44172d4ae630ec856b50520b79e0d7af41
jeremyheckt <jeremy@jeremyheckt.com>
2021-11-19 22:46:24 -0800

    added function to get current date, and function to hash a string

--- parserpc.py
+++ parserpc.py
@@ -1,4 +1,4 @@
-import json, logging
+import json, logging, datetime, hashlib
 
 class parseRPC():
     def __init__(self) -> None:
@@ -43,3 +43,20 @@ class parseRPC():
             peers_arr.append(self.dict_to_csv(peer))
 
         return peers_arr
+
+
+    def get_current_datetime(self):
+        """
+        Returns current datetime in lazy format
+        Mon Dec 31 17:41:00 2018
+        """
+        return str(datetime.datetime.now().strftime('%c'))
+
+    
+    def hash_values(self, values):
+        """
+        Takes in a csv of values (row) you wish you hash
+        Returns a tuple: (values, sha256sum of values)
+        """
+
+        return (values, str(hashlib.sha256(values).hexdigest()))
