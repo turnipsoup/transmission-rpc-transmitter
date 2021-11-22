a5df8e368f56c91a9aac6363e80f1433c4a1f9fc
jeremyheckt <jeremy@jeremyheckt.com>
2021-11-22 13:58:20 -0800

    updated ipynb file, did one final db pull

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s |%(levelname)s| %(message)s',
 
 
 URL = "http://192.168.0.250:9091/transmission/rpc"
-AUTH = ("admin", "<fancy-password-goes-here>")
+AUTH = ("admin", "Jsquad42069")
 
 def main() -> None:
     logging.info("Starting up")
